e513d80731718b78fa773afa7e62ddb2d61d1d81
Büşra Temizyürek <busratemizyureek@gmail.com>
2021-01-23 19:00:44 +0300

    Add project files.

---
+++ Odev5/MyDictionary.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev5
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;            //Bu kısımda ne yapmış olduk?
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            for (int j = 0; j < tempKeys.Length; j++)
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException("Key cannot be found");
+                }
+                else if (tempKeys[j].Equals(key))
+                {
+                    throw new ArgumentException("Same key cannot use for different values.");
+                }
+
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+            //Dictionary class ı için iki kural var:
+            //1- Farklı 2 value için aynı key kullanılamaz
+            //2- Key null (boş) olamaz
+            //Bu durumlarda programın hata vermesi gerekir. 
+        }
+
+        public void All()
+        {
+            int i = 0;
+
+            while (i < keys.Length)
+            {
+                Console.WriteLine(keys[i] + " : " + values[i]);
+
+                i++;
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Odev5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> products = new MyDictionary<int, string>();
+            products.Add(1, "Apple");
+            products.Add(2, "Banana");
+            products.Add(3, "Watermelon");
+            products.Add(4, "Mellon");
+            products.Add(5, "Kiwi");
+            products.Add(6, "Orange");
+            products.Add(7, "Pineapple");
+
+            products.All();
+        }
+    }
+}
